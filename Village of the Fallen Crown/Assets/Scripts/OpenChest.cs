e2b8c1496850b8253487895267c9fb8003dddb19
Sharif2005005 <sh2209291@qu.edu.qa>
2026-04-29 03:48:04 +0300

    lvl 2 improved

---
+++ Village of the Fallen Crown/Assets/Scripts/OpenChest.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class ChestOpen : MonoBehaviour
+{
+    public Transform chestTop;
+    public GameObject shield;
+
+    public float openAngle = -70f;
+    public float openSpeed = 3f;
+
+    private bool playerNear = false;
+    private bool isOpening = false;
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+
+    void Start()
+    {
+        closedRotation = chestTop.localRotation;
+        openRotation = closedRotation * Quaternion.Euler(openAngle, 0, 0);
+
+        if (shield != null)
+            shield.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (playerNear && Input.GetKeyDown(KeyCode.E))
+        {
+            isOpening = true;
+        }
+
+        if (isOpening)
+        {
+            chestTop.localRotation = Quaternion.Lerp(
+                chestTop.localRotation,
+                openRotation,
+                Time.deltaTime * openSpeed
+            );
+
+            if (Quaternion.Angle(chestTop.localRotation, openRotation) < 2f)
+            {
+                chestTop.localRotation = openRotation;
+
+                if (shield != null)
+                    shield.SetActive(true);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNear = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNear = false;
+        }
+    }
+}
